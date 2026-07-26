ca3ab782ca509745f429ee7deb3394b43a67659a
Haadi Khan <haadiahmerkhan@gmail.com>
2026-07-26 07:58:56 -0700

    fixed victory screen not appearing

--- Assets/Scenes/BossScene/VictoryScreen.cs
+++ Assets/Scenes/BossScene/VictoryScreen.cs
@@ -6,12 +6,12 @@ public class VictoryScreen : MonoBehaviour
     public GameObject VictoryUI;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    void OnEnable()
+    void Awake()
     {
         Boss.OnBossDefeated += ShowVictoryUI;
     }
 
-    void OnDisable()
+    void OnDestroy()
     {
         Boss.OnBossDefeated -= ShowVictoryUI;
     }
