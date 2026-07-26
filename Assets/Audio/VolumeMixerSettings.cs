9943609a6cf0712d30fb3ccc8b27a8dab39e7511
Krishna Tholudur <krishna.tholudur@gmail.com>
2026-07-26 02:35:59 -0700

    stuff

--- Assets/Audio/VolumeMixerSettings.cs
+++ Assets/Audio/VolumeMixerSettings.cs
@@ -13,22 +13,23 @@ public class VolumeMixerSettings : MonoBehaviour
 
     void Start()
     {
-        InitSlider(masterSlider, "MasterVolume");
-        InitSlider(musicSlider, "MusicVolume");
-        InitSlider(sfxSlider, "SFXVolume");
-
-        masterSlider.onValueChanged.AddListener(value => SetVolume("MasterVolume", value));
-        musicSlider.onValueChanged.AddListener(value => SetVolume("MusicVolume", value));
-        sfxSlider.onValueChanged.AddListener(value => SetVolume("SFXVolume", value));
+        Bind(masterSlider, "MasterVolume");
+        Bind(musicSlider, "MusicVolume");
+        Bind(sfxSlider, "SFXVolume");
     }
 
-    void InitSlider(Slider slider, string param)
+    void Bind(Slider slider, string param)
     {
+        if (slider == null || mixer == null) return;
+
         slider.SetValueWithoutNotify(mixer.GetFloat(param, out float dB) ? Mathf.Pow(10f, dB / 20f) : 1f);
+        slider.onValueChanged.AddListener(value => SetVolume(param, value));
     }
 
     void SetVolume(string param, float value)
     {
+        if (mixer == null) return;
+
         mixer.SetFloat(param, Mathf.Log10(Mathf.Max(value, MinVolume)) * 20f);
     }
 }
